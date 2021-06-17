0348ac0abceb31a663116a943df8151b7fdc8551
Pratap Tyagi <tyagipratap111@gmail.com>
2021-06-18 00:33:59 +0530

    commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(user);
 if (process.env.MODE_ENV === "PRODUCTION") {
   app.use("/", express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 }
 
